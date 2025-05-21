0cda477f1632fd714a01191a104efbd37fac4fad
Roy Sam <rsam@starbucks.com>
2025-05-20 20:43:31 -0700

    first commit

---
+++ Algoirthms/Problems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoirthms
+{
+    public static class Problems
+    {
+        // Find if the sum of two numbers exist in the array and return the indices
+        public static int[] TwoSum(int[] numbers, int sum )
+        {
+            Dictionary<int, int> twoSum = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int compliment = sum - numbers[i];
+
+                if (twoSum.TryGetValue(compliment, out var value))
+                {
+                    return new[] { value, i };
+                }
+                else
+                {
+                    twoSum.Add(numbers[i], i);
+                }
+            }
+
+            return  new int[] {};
+        }
+    }
+}
---
+++ Algoirthms/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Algoirthms;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] result = Problems.TwoSum(new int[] {5,7,3, 8, 1}, 11);
+        Console.WriteLine("[" + result[0] + "," + result[1] + "]");
+    }
+}
